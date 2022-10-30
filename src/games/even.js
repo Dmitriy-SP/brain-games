d2936c1eab8ce56247fa1f88dce5dddca342a226
dmitriy <dmitriychereha@gmail.com>
2022-10-30 16:20:23 +0300

    Try to fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -18,11 +18,7 @@ export default (userAnswer, userName, rndNum) => {
       getWrongAnswer(userAnswer, userName, 'yes');
       break;
     default:
-      if (rndNum % 2 === 0) {
-        getWrongAnswer(userAnswer, userName, 'no');
-      } else {
-        getWrongAnswer(userAnswer, userName, 'yes');
-      }
+      console.log(`'${userAnswer}' is wrong answer ;(.\nLet's try again, ${userName}`);
   }
 
   return false;
